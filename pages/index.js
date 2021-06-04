f01bf843309d852c721652d158120c592973db13
Devin Gong <devingong@cherrypicks.com>
2021-06-04 17:02:06 +0800

    add link

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import React, { useState, useEffect, useCallback, useContext } from 'react';
-
+import Link from "next/link"
 
 export default function HomePage() {
     return (
         <div>
             <h2>gwl002.github.io</h2>
+            <Link href="/greedySnake">贪吃蛇小游戏</Link>
         </div>
     )
 }
