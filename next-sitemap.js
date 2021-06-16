a0b1a726d1a85aa6392f2421310f7434dc341f09
Devin Gong <devingong@cherrypicks.com>
2021-06-16 21:29:48 +0800

    add seo

---
+++ next-sitemap.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://gwl002.github.io/',
+    generateRobotsTxt: true, // (optional)
+    sourceDir: "dist"
+}
--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-// import favicon from '../data/imageExports';
+const isProduction = process.env.NODE_ENV === "production";
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
@@ -15,6 +15,26 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="shortcut icon" href="/favicon.ico?" />
                     <link href="https://cdn.staticfile.org/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+                    {isProduction && (
+                        <>
+                            <script
+                                async
+                                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+                            />
+                            <script
+                                // eslint-disable-next-line react/no-danger
+                                dangerouslySetInnerHTML={{
+                                    __html: `
+                                        window.dataLayer = window.dataLayer || [];
+                                        function gtag(){dataLayer.push(arguments);}
+                                        gtag('js', new Date());
+                                        gtag('config', ${process.env.GA_TRACKING_ID});
+                                    `
+
+                                }}
+                            />
+                        </>
+                    )}
                 </Head>
                 <body>
                     <Main />
