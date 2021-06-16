1ab40b93d8654d3bfe3efc674668230517f7f428
Devin Gong <devingong@cherrypicks.com>
2021-06-16 21:41:02 +0800

    update ga

--- pages/_document.js
+++ pages/_document.js
@@ -25,12 +25,11 @@ export default class MyDocument extends Document {
                                 // eslint-disable-next-line react/no-danger
                                 dangerouslySetInnerHTML={{
                                     __html: `
-                                        window.dataLayer = window.dataLayer || [];
-                                        function gtag(){dataLayer.push(arguments);}
-                                        gtag('js', new Date());
-                                        gtag('config', ${process.env.GA_TRACKING_ID});
+                                    window.dataLayer = window.dataLayer || [];
+                                    function gtag(){dataLayer.push(arguments);}
+                                    gtag('js', new Date());
+                                    gtag('config', '${process.env.GA_TRACKING_ID}');
                                     `
-
                                 }}
                             />
                         </>
