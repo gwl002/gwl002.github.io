4e642099363201165fb3238c5227f705edf87a50
gwl002 <gong.wenlan@163.com>
2021-12-23 13:04:20 +0000

    Deploying to publish from @ gwl002/gwl002.github.io@cc4e80c2895f93b1392c55afe422f68b2f8b664d 🚀

--- _next/static/next-build/_ssgManifest.js
+++ _next/static/next-build/_ssgManifest.js
@@ -1,1 +1,1 @@
-self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Farchives","\u002Fgame","\u002F","\u002Ftag","\u002Farticle\u002F[title]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
+self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Farchives","\u002Fgame","\u002F","\u002Ftag","\u002Farticle\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
